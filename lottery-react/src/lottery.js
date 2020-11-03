bcfff97cd631737cc836ddc56fb9c5bd013230e0
Eeshan Dutta <eeshandutta21@gmail.com>
2020-11-03 22:09:49 +0530

    Smart Contract added

--- lottery-react/src/lottery.js
+++
@@ -1,7 +0,0 @@
-import web3 from './web3';
-
-const address = '0xb41f7d1de963ed73822Fc8a097CF829e8DEdb860';
-const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"enter","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"manager","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pickWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"players","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
-
-export default new web3.eth.Contract(abi , address);
-
